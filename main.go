e4126678fce061629505041f42232dc957b82143
Dasadno <Yakaleks96@gmail.com>
2025-05-20 20:12:30 +0500

    Todo complited

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Task struct {
 	Title     string `json:"title"`
-	Content   string `json:content`
-	CheckMark bool   `json:CheckMark`
-	Id        int    `json:id`
+	CheckMark bool   `json:"checkMark"`
+	Id        int    `json:"id"`
 }
 
 func GetHandler(c *gin.Context) {
 	c.JSON(200, tasks)
+}
+
+func DeleteHandler(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	for i, t := range tasks {
+		if t.Id == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			c.JSON(http.StatusOK, gin.H{"message": "Task deleted"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
 }
 
 func PostHandler(c *gin.Context) {
@@ -30,10 +49,10 @@ func PostHandler(c *gin.Context) {
 }
 
 var tasks = []Task{
-	{Id: 1, Title: "Покупка хлеба", Content: "Купить 2 буханки хлеба", CheckMark: false},
-	{Id: 2, Title: "Покупка яиц", Content: "Купить 2 стака яиц", CheckMark: false},
-	{Id: 3, Title: "Покупка альфредычей", Content: "Купить альфредыча", CheckMark: false},
-	{Id: 4, Title: "Покупка водки", Content: "Купить водки на пенсию альфредыча", CheckMark: false},
+	{Id: 1, Title: "Покупка хлеба", CheckMark: false},
+	{Id: 2, Title: "Покупка яиц", CheckMark: false},
+	{Id: 3, Title: "Покупка альфредычей", CheckMark: false},
+	{Id: 4, Title: "Покупка водки", CheckMark: false},
 }
 
 func main() {
@@ -42,5 +61,6 @@ func main() {
 	r.StaticFile("/", "./index.html")
 	r.GET("/tasks", GetHandler)
 	r.POST("/tasks", PostHandler)
+	r.DELETE("/tasks/:id", DeleteHandler)
 	r.Run()
 }
